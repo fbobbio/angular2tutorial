88181b93cccc97cd9856214a0063a48b0e09d3fd
fbobbio <federicobobbio@gmail.com>
2016-08-05 18:21:25 +0200

    Uploading sources for the finished Angular2 sample

---
+++ app/app.routes.ts
@@ -0,0 +1,28 @@
+import { provideRouter, RouterConfig }  from '@angular/router';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+const routes: RouterConfig = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
+
+export const appRouterProviders = [
+  provideRouter(routes)
+];
